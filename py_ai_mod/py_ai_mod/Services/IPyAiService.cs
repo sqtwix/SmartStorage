da0fa854c5df67833f21551d0d314144ccbbeea1
SpunchBob <aleksandrprokudin2007@gmail.com>
2025-10-20 20:03:01 +0300

    py_ai_mod asp.net part v0.1

---
+++ py_ai_mod/py_ai_mod/Services/IPyAiService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using py_ai_mod.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace py_ai_mod.Services
+{
+    public interface IPyAiService
+    {
+        Task<string> PredictAsync(string inputText);
+        Task<bool> IsHealthyAsync();
+        Task<PyAiModule> GetModuleInfoAsync();
+    }
+}
