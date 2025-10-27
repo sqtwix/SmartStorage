40f635a5c037930f273e3607a8213ea6f9d66dbe
sqtwix <ivan20140767@gmail.com>
2025-10-27 23:20:28 +0300

    Добавлен метод current (получение информации о складе)

--- SmartStorageBackend/SmartStorageBackend/Controllers/DashbordController.cs
+++ SmartStorageBackend/SmartStorageBackend/Controllers/DashbordController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using SmartStorageBackend;
 using SmartStorageBackend.Hubs;
 
@@ -21,5 +22,62 @@ public class DashboardController : ControllerBase
     {
         await _hub.Clients.All.SendAsync("inventory_alert", new { message, time = DateTime.UtcNow });
         return Ok(new { status = "alert_sent" });
+    }
+
+    [HttpGet("current")]
+    public async Task<IActionResult> GetCurrentState()
+    {
+        // Информация о роботах
+        var robots = await _db.Robots
+            .Select(r => new
+            {
+                r.Id,
+                r.Status,
+                r.BatteryLevel,
+                r.CurrentZone,
+                r.CurrentRow,
+                r.CurrentShelf,
+                r.LastUpdate
+            }).ToListAsync();
+
+        // Информация о последних сканах
+        var recentScans = await _db.InventoryHistory
+           .OrderByDescending(h => h.ScannedAt)
+           .Take(20)
+           .Select(h => new
+           {
+               h.Id,
+               h.RobotId,
+               h.ProductId,
+               h.Quantity,
+               h.Zone,
+               h.RowNumber,
+               h.ShelfNumber,
+               h.Status,
+               h.ScannedAt
+           })
+           .ToListAsync();
+
+        // Статистика
+        var stats = new
+        {
+            total_products = await _db.Products.CountAsync(),
+            total_scans = await _db.InventoryHistory.CountAsync(),
+            critical_products = await _db.InventoryHistory
+                .Where(h => h.Status == "CRITICAL")
+                .Select(h => h.ProductId)
+                .Distinct()
+                .CountAsync(),
+            active_robots = robots.Count(r => r.Status == "active"),
+            last_update = DateTime.UtcNow
+        };
+
+        return Ok(new
+        {
+            robots,
+            recentScans,
+            stats
+        }
+        );
     }
 }
