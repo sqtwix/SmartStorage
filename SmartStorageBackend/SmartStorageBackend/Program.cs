9e64375ce820d5ffedd9615c38befc659fa184b7
sqtwix <ivan20140767@gmail.com>
2025-10-28 21:00:09 +0300

    Добавлена документация swagger

--- SmartStorageBackend/SmartStorageBackend/Program.cs
+++ SmartStorageBackend/SmartStorageBackend/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SmartStorageBackend;
-using Microsoft.EntityFrameworkCore.Design;
 using SmartStorageBackend.Hubs;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,41 +9,72 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SmartStorageContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// JWT и контроллеры
+// Добавляем контроллеры и Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SmartStorage API",
+        Version = "v1",
+        Description = "Backend для системы SmartStorage (роботы, склад, ИИ)",
+        Contact = new OpenApiContactdot
+        {
+            Name = "Backend Team",
+            Email = "ivan20140767@gmail.com.com"
+        }
+    });
 
+    // Поддержка авторизации (JWT)
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Введите JWT токен (пример: Bearer eyJhbGci...)",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 // Добавляем SignalR
 builder.Services.AddSignalR();
 
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
+// Swagger UI и OpenAPI JSON
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartStorage API v1");
+        c.RoutePrefix = ""; 
+    });
+}
+
+app.UseHttpsRedirection();
 app.UseRouting();
+app.UseAuthorization();
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
     endpoints.MapHub<DashboardHub>("/api/ws/dashboard");
 });
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
