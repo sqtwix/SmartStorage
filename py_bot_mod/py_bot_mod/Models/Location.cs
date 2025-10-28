1c3d198c8c5f7331320729a17da2983d6a059a9d
SpunchBob <aleksandrprokudin2007@gmail.com>
2025-10-28 16:07:02 +0300

    py_bot_mod v0.1

---
+++ py_bot_mod/py_bot_mod/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace py_bot_mod.Models
+{
+    public class Location
+    {
+        public string Zone { get; set; }
+        public int Row { get; set; }
+        public int Shelf { get; set; }
+    }
+}
